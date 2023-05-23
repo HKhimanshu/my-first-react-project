434decf66e2b6ea39f0e5f2fd9d2c45d73878b2e
HKhimanshu <himanshu773962@gmail.com>
2023-05-23 15:59:15 +0530

    updated order folder

--- src/order/order.js
+++ src/order/order.js
@@ -9,16 +9,29 @@ function ProductOrder() {
           <h2>All orders</h2>
           <p>from anytime</p>
         </div>
-        <div>
+        <div className={style.filter}>
+          <div className={style.search}></div>
           <div>
-            <div className={style.search}></div>
+            <button className={style.content}>filter</button>
+          </div>
+        </div>
+      </div>
+      <div className={style.order}>
+        <div className={style.person}>
+          <p className={style.packed}>packed</p>
+          <p>Arriving by Fri, 2 Jun</p>
+          <div className={style.details}>
             <div>
-              <button>filter</button>
+              <img src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15760662/2021/10/7/05de2226-2df8-4c6a-8572-25f56f7b34fe1633612031245Cold-pressedWhiteSesameOil250ml1.jpg"></img>
+            </div>
+            <div className={style.size}>
+              <b>ESSENTIA EXTRACTS</b>
+              <p>Cold-Pressed White Sesame Oil-250ml</p>
+              <p>size:250ml</p>
             </div>
           </div>
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
